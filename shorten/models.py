ee401a6dcd419e645ec1080a150802c7e7a6b562
Daniel Malqui <daniel144.10@gmail.com>
2022-10-07 23:28:26 -0500

    first commit

---
+++ shorten/models.py
@@ -0,0 +1,7 @@
+from unittest.util import _MIN_COMMON_LEN
+from django.db import models
+
+# Create your models here.
+class URL(models.Model):
+    link = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=10)
